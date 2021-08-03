d2508c1abac013643a27d6c23eab679f2275f3d3
Vaibhav Raj Mishra <81673399+raje-7@users.noreply.github.com>
2021-08-03 16:07:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// For Carousel Effect in Blog posts
+
+$(document).ready(function () {
+    $('.All-blogs').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '#prev',
+        nextArrow: '#next',
+    });
+});
